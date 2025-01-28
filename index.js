340f2669e870cc5748ae8b2995fc4fe2f519daae
momo <mohamedfountir@gmail.com>
2025-01-28 10:04:35 +0100

    Quick fix responsive

---
+++ index.js
@@ -0,0 +1,8 @@
+// faire fonctioner le bouton de la navbar
+// Quand on clique sur le bouton, on redirige vers la page d'authentification
+const btnBuyNav = document.querySelector(".btnBuyNav");
+btnBuyNav.addEventListener("click", () => {
+    window.location.href = "authentification.html";
+});
+
+
