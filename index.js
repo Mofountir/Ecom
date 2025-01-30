3738e8a4b5b0aa8dc345876f6ffb5ece4b09e6f5
momo <mohamedfountir@gmail.com>
2025-01-30 13:33:58 +0100

    toggle manuel ok

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// faire fonctioner le bouton de la navbar
-// Quand on clique sur le bouton, on redirige vers la page d'authentification
-const btnBuyNav = document.querySelector(".btnBuyNav");
-btnBuyNav.addEventListener("click", () => {
-    window.location.href = "authentification.html";
+const menuToggle = document.querySelector('.container');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
 });
+
+
 
 
